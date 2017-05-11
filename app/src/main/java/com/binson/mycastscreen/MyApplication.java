178f9138d39af294017c006db874fa67ccc263ad
BinsonZoom <binsonzoom@163.com>
2017-05-11 21:21:55 +0800

    Initial commit

---
+++ app/src/main/java/com/binson/mycastscreen/MyApplication.java
@@ -0,0 +1,15 @@
+package com.binson.mycastscreen;
+
+import android.app.Application;
+
+/**
+ * Created by zoubingshun on 2017/5/8.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Utils.init(this);
+    }
+}
